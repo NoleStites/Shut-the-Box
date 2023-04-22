749624416c4cfd0daa755aa8ebc8b64cd5ab97ad
Nole Stites <nolesters@Life-of-Macbeth>
2023-04-21 19:52:08 -0700

    Score screen complete

--- view/View.py
+++ view/View.py
@@ -13,9 +13,9 @@ class View():
         """
 
         # Initializing the root window
-        root = Tk()
-        root.geometry("600x400")    # Dimensions of the frame (window)
-        root.title("Shut the Box")  # Used to add a title to the whole window
+        self.root = Tk()
+        self.root.geometry("600x400")    # Dimensions of the frame (window)
+        self.root.title("Shut the Box")  # Used to add a title to the whole window
 
         # Initializing variables
         self.controller = controller
@@ -24,65 +24,65 @@ class View():
         self.active_tiles = []  # A list of the tiles currently selected by the player
 
         # Top of root window
-        topframe = Frame(root, height=200, width=300, bg="#331a00")  # 'root' refers to the parent frame that this frame is inside. Root is the main window.
-        topframe.pack(expand=True, fill=BOTH)
+        self.topframe = Frame(self.root, height=200, width=300, bg="#331a00")  # 'root' refers to the parent frame that this frame is inside. Root is the main window.
+        self.topframe.pack(expand=True, fill=BOTH)
     
         # Tiles
-        self.tile1 = Checkbutton(topframe, text="1", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[0], state=DISABLED)
+        self.tile1 = Checkbutton(self.topframe, text="1", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[0], state=DISABLED)
         self.tile1.place(relx=0.01, rely=0.3)
 
-        self.tile2 = Checkbutton(topframe, text="2", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[1], state=DISABLED)
+        self.tile2 = Checkbutton(self.topframe, text="2", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[1], state=DISABLED)
         self.tile2.place(relx=0.12, rely=0.3)
 
-        self.tile3 = Checkbutton(topframe, text="3", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[2], state=DISABLED)
+        self.tile3 = Checkbutton(self.topframe, text="3", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[2], state=DISABLED)
         self.tile3.place(relx=0.23, rely=0.3)
 
-        self.tile4 = Checkbutton(topframe, text="4", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[3], state=DISABLED)
+        self.tile4 = Checkbutton(self.topframe, text="4", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[3], state=DISABLED)
         self.tile4.place(relx=0.34, rely=0.3)
 
-        self.tile5 = Checkbutton(topframe, text="5", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[4], state=DISABLED)
+        self.tile5 = Checkbutton(self.topframe, text="5", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[4], state=DISABLED)
         self.tile5.place(relx=0.45, rely=0.3)
 
-        self.tile6 = Checkbutton(topframe, text="6", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[5], state=DISABLED)
+        self.tile6 = Checkbutton(self.topframe, text="6", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[5], state=DISABLED)
         self.tile6.place(relx=0.56, rely=0.3)
 
-        self.tile7 = Checkbutton(topframe, text="7", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[6], state=DISABLED)
+        self.tile7 = Checkbutton(self.topframe, text="7", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[6], state=DISABLED)
         self.tile7.place(relx=0.67, rely=0.3)
 
-        self.tile8 = Checkbutton(topframe, text="8", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[7], state=DISABLED)
+        self.tile8 = Checkbutton(self.topframe, text="8", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[7], state=DISABLED)
         self.tile8.place(relx=0.78, rely=0.3)
 
-        self.tile9 = Checkbutton(topframe, text="9", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[8], state=DISABLED)
+        self.tile9 = Checkbutton(self.topframe, text="9", height=3, width=1, bg="#a6a6a6", activebackground="#bfbfbf", fg="black", font=("Arial", 16, "bold"), relief=RAISED, command=self.tileChoiceChange, variable=self.tile_states[8], state=DISABLED)
         self.tile9.place(relx=0.89, rely=0.3)
 
 
         # Bottom of root window
-        bottomframe = Frame(root, height=200, width=300, bg="#004d00")
-        bottomframe.pack(expand=True, fill=BOTH)
+        self.bottomframe = Frame(self.root, height=200, width=300, bg="#004d00")
+        self.bottomframe.pack(expand=True, fill=BOTH)
 
         # Dice roll 1 button
-        self.diceroll1_button = Button(bottomframe, height=2, width=10, bg="black", fg="gray", activebackground="#1a1a1a", activeforeground="gray", text="Roll One Dice", font=("Arial", 10, "italic"), command=self.generateRoll1)
+        self.diceroll1_button = Button(self.bottomframe, height=2, width=10, bg="black", fg="gray", activebackground="#1a1a1a", activeforeground="gray", text="Roll One Dice", font=("Arial", 10, "italic"), command=self.generateRoll1)
         self.diceroll1_button.place(relx=0.1, rely=0.15)
 
         # Dice roll 2 button
-        self.diceroll2_button = Button(bottomframe, height=2, width=10, bg="black", fg="gray", activebackground="#1a1a1a", activeforeground="gray", text="Roll Two Dice", font=("Arial", 10, "italic"), command=self.generateRoll2)
+        self.diceroll2_button = Button(self.bottomframe, height=2, width=10, bg="black", fg="gray", activebackground="#1a1a1a", activeforeground="gray", text="Roll Two Dice", font=("Arial", 10, "italic"), command=self.generateRoll2)
         self.diceroll2_button.place(relx=0.7, rely=0.15)
 
         # Cover tiles button
-        self.covertiles_button = Button(bottomframe, height=2, width=10, bg="#004d00", fg="white", activebackground="#800000", activeforeground="black", text="Cover Tiles", font=("Arial", 10, "italic"), state=DISABLED, command=self.coverTiles)
+        self.covertiles_button = Button(self.bottomframe, height=2, width=10, bg="#004d00", fg="white", activebackground="#800000", activeforeground="black", text="Cover Tiles", font=("Arial", 10, "italic"), state=DISABLED, command=self.coverTiles)
         self.covertiles_button.place(relx=0.39, rely=0.6)
 
         # Dice results
-        diceresults = Frame(bottomframe, height=73, width=73, bg="#003300")
+        diceresults = Frame(self.bottomframe, height=73, width=73, bg="#003300")
         diceresults.place(relx=0.43, rely=0.1)
         self.dicelabel = Label(diceresults, text="", fg="white", bg="#003300", font=("Arial", 16, "bold"))
         self.dicelabel.place(relx=0.3, rely=0.3)
 
         # End game button
-        self.endgame_button = Button(bottomframe, height=1, width=6, bg="#cc7a00", fg="black", text="End Game", command=self.endGame)
+        self.endgame_button = Button(self.bottomframe, height=1, width=6, bg="#cc7a00", fg="black", text="End Game", command=self.endGame)
         self.endgame_button.place(relx=0.87, rely=0.86)
 
-        root.mainloop() # Starts/runs the GUI (everything involving the GUI should come before this)
+        self.root.mainloop() # Starts/runs the GUI (everything involving the GUI should come before this)
 
 
     def tileChoiceChange(self):
@@ -263,5 +263,36 @@ class View():
             state_values.append(tile.get())
 
         score = self.controller.calculateScore(state_values)
+        self.scoreScreen(score)
 
-        print(f'SCORE: {score}')
+
+    def scoreScreen(self, score):
+        """
+        Clears the window and displays the score
+        """
+        # Clear the game window
+        self.topframe.destroy()
+        self.bottomframe.destroy()
+
+        # Display the score
+        score_message = f'Score: {score}'
+
+        endframe = Frame(self.root, height=400, width=600, bg="#999966")  # 'root' refers to the parent frame that this frame is inside. Root is the main window.
+        endframe.pack(expand=True, fill=BOTH)
+        scorelabel = Label(endframe, text=score_message, font=("Comic Sans", 36, "bold"), bg="#999966")
+        scorelabel.place(relx=0.28, rely=0.2)
+
+        # Exit button
+        exitbutton = Button(endframe, command=self.terminate, text="EXIT", font=("Comic Sans", 20), bg="black", fg="white", height=2, width=4, activebackground="#1a1a1a", activeforeground="white")
+        exitbutton.place(relx=0.42, rely=0.6)
+
+
+    def terminate(self):
+        """
+        Terminates the game completely.
+        """
+        self.root.quit()
+
+
+
+
